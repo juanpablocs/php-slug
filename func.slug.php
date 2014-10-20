4f6a3e71ed9e255b34b723668245b6657e6c41c6
Juan Pablo <jpmaster.net@gmail.com>
2014-10-19 20:10:55 -0500

    edited func.slug.php

--- func.slug.php
+++ func.slug.php
@@ -1,3 +1,17 @@
 <?php
+//function slug
+function slug($string, $slug='-', $maxlength=50){
+        $string = strip_tags($string);
+        $seo = strtolower(trim(preg_replace('~[^0-9a-z]+~i', $slug, html_entity_decode(preg_replace('~&([a-z]{1,2})(?:acute|cedil|circ|grave|lig|orn|ring|slash|th|tilde|uml);~i', '$1', htmlentities($string, ENT_QUOTES, 'UTF-8')), ENT_QUOTES, 'UTF-8')), $slug));
+        
+        if(strlen($seo) > $maxlength)
+        {
+        	$seo = substr($seo, 0, $maxlength);
+        	$pos = (int)strrpos($seo, $slug); //ultima posicion de la busqueda
+        	if($pos)
+        		$seo = substr($seo, 0, $pos);       	
+        }
 
-print 't';
+        	
+        return $seo;
+}
